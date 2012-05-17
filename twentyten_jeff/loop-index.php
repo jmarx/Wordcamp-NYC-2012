58b65860eefe3739ae536ad4bfee1caa87275b3d
Jeff Marx <jeffrey_marx@yahoo.com>
2012-05-17 16:54:55 -0400

    indenting code better, using a better method for determining if counter number is odd or even, and setting empty divs for the category boxes

--- twentyten_jeff/loop-index.php
+++ twentyten_jeff/loop-index.php
@@ -48,21 +48,30 @@ $i = 0;
 
 //start the loop
 while ( $secondaryfeatured->have_posts() ) : $secondaryfeatured ->the_post();
-
-//add a counter each time so we can keep track of the posts
-$i++;
-
-//if the post counter is equal to zero, which is the first one, align it to the left, otherwise align it to the right
-if ($i&1) {$align = "alignleft"; } else {$align = "alignright";}
-// put your html under here
- ?>
-<div>
-<?php the_post_thumbnail('thumbnail', array('class' => $align)); ?>
-<h3 style="display:inline;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-<div class="excerpt"><?php the_excerpt(); ?></div>
-</div>
-<div style="clear:both; height:3px;">&nbsp;</div>
+	//add a counter each time so we can keep track of the posts
+	$i++;
+	//if the post counter is equal to zero, which is the first one, align it to the left, otherwise align it to the right
+	if ($i%2) {
+			$align = "alignleft";
+		} else {
+			$align = "alignright";
+			}
+	// put your html under here
+	 ?>
+	<div>
+		<?php the_post_thumbnail('thumbnail', array('class' => $align)); ?>
+		<h3 style="display:inline;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<div class="excerpt"><?php the_excerpt(); ?></div>
+	</div>
+	<div style="clear:both; height:3px;">&nbsp;</div>
  <?php
 endwhile;
 //end the loop
 ?>
+
+<div id="categoryboxes">
+	<div id="superman"></div>
+	<div id="batman"></div>
+	<div id="spiderman"></div>
+	<div id="ironman"></div>
+</div>
