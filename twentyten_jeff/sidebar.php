df60cd547e7f080f6ac353bb651b24812fc54c91
Jeff Marx <jeffrey_marx@yahoo.com>
2012-05-23 10:59:10 -0400

    First crack at customizing the sidebar. More to come.

--- twentyten_jeff/sidebar.php
+++ twentyten_jeff/sidebar.php
@@ -8,49 +8,50 @@
  */
 ?>
 
-		<div id="primary" class="widget-area" role="complementary">
-			<ul class="xoxo">
-
-<?php
-	/* When we call the dynamic_sidebar() function, it'll spit out
-	 * the widgets for that widget area. If it instead returns false,
-	 * then the sidebar simply doesn't exist, so we'll hard-code in
-	 * some default sidebar stuff just in case.
-	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
-
-			<li id="search" class="widget-container widget_search">
-				<?php get_search_form(); ?>
-			</li>
-
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
-
-		<?php endif; // end primary widget area ?>
+	<div id="primary" class="widget-area" role="complementary">
+	<ul class="xoxo">
+		<li id="search-2" class="widget-container widget_search"><form role="search" method="get" id="searchform" action="http://wordcamp.jeffreymarx.net/" >
+			<div><label class="screen-reader-text" for="s">Search for:</label>
+			<input type="text" value="" name="s" id="s" />
+			<input type="submit" id="searchsubmit" value="Search" />
+			</div>
+			</form></li>
+		<li id="recent-posts-2" class="widget-container widget_recent_entries">
+			<h3 class="widget-title">Recent Posts</h3>
+			<ul>
+				<li><a href="http://wordcamp.jeffreymarx.net/picture-of-the-day/" title="Picture of the day">Picture of the day</a></li>
+				<li><a href="http://wordcamp.jeffreymarx.net/random-iron-man-headline-2/" title="Random Iron Man headline">Random Iron Man headline</a></li>
+				<li><a href="http://wordcamp.jeffreymarx.net/another-random-iron-man-headline/" title="Another Random Iron Man headline">Another Random Iron Man headline</a></li>
+				<li><a href="http://wordcamp.jeffreymarx.net/random-iron-man-headline/" title="Random Iron Man headline">Random Iron Man headline</a></li>
+				<li><a href="http://wordcamp.jeffreymarx.net/another-random-spiderman-headline/" title="Another random Spiderman headline">Another random Spiderman headline</a></li>
 			</ul>
-		</div><!-- #primary .widget-area -->
+		</li>
+		<li id="recent-comments-2" class="widget-container widget_recent_comments">
+			<h3 class="widget-title">Picture of the day</h3>
+					<?php
+					// The class instantiation with arguments
+					$pic_query = new WP_Query(
+						array(
+						'tag' => 'picoftheday',
+						'post_status' => 'publish',
+						'posts_per_page' => '1',
+							)
+						);
+					//start the loop
+					while ( $pic_query ->have_posts() ) : $pic_query ->the_post();
 
-<?php
-	// A second sidebar for widgets, just because.
-	if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
+					// put your html under here
+				?>
 
-		<div id="secondary" class="widget-area" role="complementary">
-			<ul class="xoxo">
-				<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-			</ul>
-		</div><!-- #secondary .widget-area -->
+				<?php the_post_thumbnail('full'); ?>
+				<div class="cutline"><?php the_content(); ?></div>
 
-<?php endif; ?>
+				<?php
+					endwhile;
+					//end the loop
+			?>
+		</li>
+	</ul>
+	</div><!-- #primary .widget-area -->
+
+</div><!-- #main -->
